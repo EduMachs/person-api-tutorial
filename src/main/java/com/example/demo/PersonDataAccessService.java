8fbde0695361eee578ef5b62e783d3f1eacf0140
Eduardo Machado <eduardo.sca98@gmail.com>
2022-04-22 22:51:28 -0300

    First commit

---
+++ src/main/java/com/example/demo/PersonDataAccessService.java
@@ -0,0 +1,21 @@
+//package com.example.demo;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//public class PersonDataAccessService implements PersonDao {
+//
+//
+//  @Override
+//  public UUID addPerson(UUID id, Person person) {
+//    return null;
+//  }
+//
+//  @Override
+//  public List<Person> getPeople() {
+//    return null;
+//  }
+//
+//
+//}
